63594d265502b6d9c465cdb58f898e99c0828566
Simon <simongonand@gmail.com>
2021-11-19 19:29:39 +0100

    camera bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,10 @@ public class PlayerController : MonoBehaviour
 {
     public Transform self;
     public Rigidbody selfRigidbody;
+    public Transform cam;
 
     private Vector2 movements = Vector2.zero;
+    private Vector2 cameraMovements = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +23,18 @@ public class PlayerController : MonoBehaviour
         movements = context.ReadValue<Vector2>();
     }
 
+    public void OnCameraMove(InputAction.CallbackContext context)
+    {
+        cameraMovements += context.ReadValue<Vector2>();
+        //cameraMovements.x = Mathf.Clamp(cameraMovements.x, -60f, 60f);
+        //cameraMovements.y = Mathf.Clamp(cameraMovements.y, -60f, 60f);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         selfRigidbody.velocity = new Vector3(movements.x, 0.0f, movements.y);
+        self.eulerAngles = new Vector3(0.0f, cameraMovements.x, 0.0f);
+        cam.eulerAngles = new Vector3(-cameraMovements.y, 0.0f, 0.0f);
     }
 }
