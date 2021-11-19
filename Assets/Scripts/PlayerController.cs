6c195d6ea821111482caf2709f17e6eb54cf04c6
Simon <simongonand@gmail.com>
2021-11-19 17:24:21 +0100

    add player movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Transform self;
+    public Transform self;
+    public Rigidbody selfRigidbody;
+
+    private Vector2 movements = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
         Transform spawn = GameObject.FindGameObjectWithTag("Player").transform;
-        self = spawn;
+        self.position = new Vector3(spawn.position.x, self.position.y, spawn.position.z);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movements = context.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        selfRigidbody.velocity = new Vector3(movements.x, 0.0f, movements.y);
     }
 }
